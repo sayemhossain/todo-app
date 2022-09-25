0ddab4dd03c426a90bbcad2200c1f06b96d78920
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-26 01:11:36 +0600

    ne wcod

--- src/redux/todos/reducer.js
+++ src/redux/todos/reducer.js
@@ -25,7 +25,7 @@ const reducer = (state = initialState, action) => {
         }
         return {
           ...todo,
-          completed: !completed,
+          completed: !todo.completed,
         };
       });
 
@@ -59,3 +59,4 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+export default reducer;
