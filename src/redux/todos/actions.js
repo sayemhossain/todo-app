5e284c46f3d7244784a2ef703ed3e6ff8960db6f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-26 00:32:37 +0600

    new code

--- src/redux/todos/actions.js
+++ src/redux/todos/actions.js
@@ -1,8 +1,28 @@
-import { ADDED } from "./actionTypes";
+import { ADDED, COLORSELECTED, TOGGLED } from "./actionTypes";
 
+// this is for todo adding purpose
 export const added = ({ todoText }) => {
   return {
     type: ADDED,
     payload: todoText,
+  };
+};
+
+// this is for toggling between todos
+export const toggled = ({ todoId }) => {
+  return {
+    type: TOGGLED,
+    payload: todoId,
+  };
+};
+
+// this is for toggling between todos
+export const colorSelected = ({ todoId, color }) => {
+  return {
+    type: COLORSELECTED,
+    payload: {
+      todoId: todoId,
+      color: color,
+    },
   };
 };
