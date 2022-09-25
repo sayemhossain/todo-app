288b8ea867e8c27c1fd49a76bb8d5c68690cc911
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-25 23:44:26 +0600

    new code added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+import notes from "./img/notes.png";
+import doubleTick from "./img/double-tick.png";
+import cancel from "./img/cancel.png";
 
 function App() {
   return (
@@ -12,7 +15,7 @@ function App() {
         {/* <!-- header --> */}
         <div>
           <form class="flex items-center bg-gray-100 px-4 py-4 rounded-md">
-            <img src="./images/notes.png" class="w-6 h-6" alt="Add todo" />
+            <img src={notes} class="w-6 h-6" alt="Add todo" />
             <input
               type="text"
               placeholder="Type your todo"
@@ -20,17 +23,13 @@ function App() {
             />
             <button
               type="submit"
-              class="appearance-none w-8 h-8 bg-[url('./images/plus.png')] bg-no-repeat bg-contain"
+              class="appearance-none w-8 h-8 bg-[url('./img/plus.png')] bg-no-repeat bg-contain"
             ></button>
           </form>
 
           <ul class="flex justify-between my-4 text-xs text-gray-500">
             <li class="flex space-x-1 cursor-pointer">
-              <img
-                class="w-4 h-4"
-                src="./images/double-tick.png"
-                alt="Complete"
-              />
+              <img class="w-4 h-4" src={doubleTick} alt="Complete" />
               <span>Complete All Tasks</span>
             </li>
             <li class="cursor-pointer">Clear completed</li>
@@ -63,7 +62,7 @@ function App() {
             <div class="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500"></div>
 
             <img
-              src="./images/cancel.png"
+              src={cancel}
               class="flex-shrink-0 w-4 h-4 ml-2 cursor-pointer"
               alt="Cancel"
             />
@@ -92,7 +91,7 @@ function App() {
             <div class="flex-shrink-0 h-4 w-4 rounded-full border-2 ml-auto cursor-pointer border-red-500 hover:bg-red-500"></div>
 
             <img
-              src="./images/cancel.png"
+              src={cancel}
               class="flex-shrink-0 w-4 h-4 ml-2 cursor-pointer"
               alt="Cancel"
             />
