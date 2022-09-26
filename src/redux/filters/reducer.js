bb34196514ca0dd4ba225abb65983da3026c0ef8
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-09-27 00:53:48 +0600

    new code added

--- src/redux/filters/reducer.js
+++ src/redux/filters/reducer.js
@@ -1,4 +1,16 @@
+import { STATUSCHANGED } from "./actionTypes";
 import initialState from "./initialState";
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STATUSCHANGED:
+      return {
+        ...state,
+        status: action.payload,
+      };
+
+    default:
+      break;
+  }
+};
 export default reducer;
